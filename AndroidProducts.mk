c36ef101d28660f53067d60be6c5abbc3f870164
chyinan <60872708+chyinan@users.noreply.github.com>
2022-08-08 23:51:09 +0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_KONKA U17.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_KONKA U17-user \
+    omni_KONKA U17-userdebug \
+    omni_KONKA U17-eng
